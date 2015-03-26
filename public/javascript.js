86f068aa3cdffadb22067c9c2a05d174d8b3e620
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2015-03-26 17:26:34 -0500

    refactor tab code

--- public/javascript.js
+++ public/javascript.js
@@ -1,16 +1,10 @@
-$( "div" ).hide();
+$('div').hide();
+$('div[data-number=1]').show();
 
-$( "#tab1" ).on( "click", function() {
-  $('#page1').show();
-  $('#page2, #page3').hide();
-});
+$('li').on('click', function(e) {
+  var number = $(e.target).data('number');
+  
+  $('div').hide();
+  $('div[data-number="' + number + '"]').show();
+})
 
-$( "#tab2" ).on( "click", function() {
-  $('#page2').show();
-  $('#page1, #page3').hide();
-});
-
-$( "#tab3" ).on( "click", function() {
-  $('#page3').show();
-  $('#page1, #page2').hide();
-});
