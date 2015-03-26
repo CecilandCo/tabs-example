b0e990341c38732e24acb20192004c74963ed8a5
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2015-03-26 17:31:47 -0500

    Add active class to tabs

--- public/javascript.js
+++ public/javascript.js
@@ -3,8 +3,10 @@ $('div[data-number=1]').show();
 
 $('li').on('click', function(e) {
   var number = $(e.target).data('number');
-  
+
+  $('li').removeClass('active')
+  $(e.target).addClass('active')
+
   $('div').hide();
   $('div[data-number="' + number + '"]').show();
 })
-
